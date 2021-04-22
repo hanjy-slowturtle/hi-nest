9a79060b29cb7b2382f0f4cf60cf1f5b2cd47f0d
hanjy-slowturtle <h920915@gmail.com>
2021-04-22 22:29:26 +0900

    ~ #1 ARCHITECTURE OF NESTJS

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello NestJS!';
+  }
+
+  getHi(): string {
+    return 'Hi NestJS!';
   }
 }
